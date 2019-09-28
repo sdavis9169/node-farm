cbdf7033e1362679d34367a9d961b0f678cefb8f
Sdavis <stevendavis0622@gmail.com>
2019-09-28 14:36:08 -0600

    moved module to its own folder

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const  replaceTemplate  = require('./modules/replaceTemplate');
+ 
+
 // BLOCKING CODE :-(
 // const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 // console.log(textIn)
@@ -24,21 +27,7 @@ const url = require('url');
 // console.log('Will read file');
 
 ////////////////
-// SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
+// SERVER //
 const tempOverview = fs.readFileSync(`${__dirname}/pages/overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/pages/card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/pages/product.html`, 'utf-8');
